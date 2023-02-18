043dd3f17552693bfe8a1f6461ecb7ca654fb977
ozdemirozcelik <ozdemirozcelik@gmail.com>
2023-02-17 19:06:19 -0500

    bugfix before week 4

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -192,8 +192,8 @@ class TestAccountService(TestCase):
             'X-Frame-Options': 'SAMEORIGIN',
             'X-XSS-Protection': '1; mode=block',
             'X-Content-Type-Options': 'nosniff',
-            'Content-Security-Policy': 'default-src \'self\';\
-                object-src \'none\'',
+            'Content-Security-Policy': 'default-src \'self\'; '
+            'object-src \'none\'',
             'Referrer-Policy': 'strict-origin-when-cross-origin'
         }
         for key, value in headers.items():
